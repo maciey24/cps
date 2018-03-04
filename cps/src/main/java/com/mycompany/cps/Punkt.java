284717a9fc0331331541e9fc9401544eac7a90ae
Maciej Ślusarz <189789@edu.p.lodz.pl>
2018-03-04 23:28:14 +0100

    rysowanie wykresów

--- cps/src/main/java/com/mycompany/cps/Punkt.java
+++ cps/src/main/java/com/mycompany/cps/Punkt.java
@@ -13,10 +13,39 @@ import java.util.List;
  * @author maciek
  */
 public class Punkt {
-    ArrayList<Double> wspolrzedne;
+    private ArrayList<Double> wspolrzedne;
+
+    public void setWspolrzedne(ArrayList<Double> wspolrzedne) {
+        this.wspolrzedne = wspolrzedne;
+    }
+
+    public ArrayList<Double> getWspolrzedne() {
+        return wspolrzedne;
+    }
     
     Punkt()
     {
         this.wspolrzedne = new ArrayList<>();
     }
+    
+    Punkt(List<Double> listaWspolrzednych)
+    {
+        this.wspolrzedne = (ArrayList<Double>) listaWspolrzednych;
+    }
+    
+    Punkt(int x, int y)
+    {
+        this.wspolrzedne = new ArrayList<>();
+        this.wspolrzedne.add(Double.parseDouble(String.valueOf(x)));
+        this.wspolrzedne.add(Double.parseDouble(String.valueOf(y)));
+    }
+    
+    Punkt(Double x, Double y)
+    {
+        this.wspolrzedne = new ArrayList<>();
+        this.wspolrzedne.add(x);
+        this.wspolrzedne.add(y);
+    }
+    
+    
 }
--- cps/src/main/java/com/mycompany/cps/zadJ.java
+++ cps/src/main/java/com/mycompany/cps/zadJ.java
@@ -5,13 +5,39 @@
  */
 package com.mycompany.cps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author maciek
  */
 public class zadJ {
+
+    static Double czestProbk = 0.01;
+    static Double gornaGranica = Math.PI*2;
+
+    static ArrayList<List<Punkt>> listyPunktow = new ArrayList<>();
+    static ArrayList<String> etykiety = new ArrayList<>();
+
     public static void main(String[] args) {
+        etykiety.add("sin");
+        etykiety.add("cos");
         
+        ArrayList punkty = new ArrayList();
+        for (Double i = 0.0; i < gornaGranica; i = i + czestProbk) {
+            punkty.add(new Punkt(i, Math.sin(i)));
+        }
+        listyPunktow.add(punkty);
+        
+        punkty = new ArrayList();
+        for (Double i = 0.0; i < gornaGranica; i = i + czestProbk) {
+            punkty.add(new Punkt(i, Math.cos(i)));
+        }
+        listyPunktow.add(punkty);
+
+        
+        Wykres.rysuj("Tytuł okna", "Wykres", listyPunktow, etykiety);
     }
-    
+
 }
